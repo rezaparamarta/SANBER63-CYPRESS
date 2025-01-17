81a414c8b6d6006c6a38b2520ecee6679a51377c
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-17 22:49:07 +0700

    Add git ignore cypress e2e

--- cypress/e2e/web-test/testfixtures.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Login using fixture', () => {
-    it('Success login', () =>{
-        cy.fixture('Users.json').then((users)=>{
-            const datauser = user[0];
-            cy.loginSaucedemo(datauser.username)
-        });
-    });
-});
