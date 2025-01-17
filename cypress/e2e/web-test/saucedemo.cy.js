dc7145a73d1c7544025794a6ec1dab435bc2e30e
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-18 01:51:25 +0700

    Add syntax functionality

--- cypress/e2e/web-test/saucedemo.cy.js
+++ cypress/e2e/web-test/saucedemo.cy.js
@@ -1,21 +1,21 @@
-describe('Test Suite 1', () => {
+describe('Saucedemo Login Scenarios', () => {
     beforeEach(() => {
         cy.visit('');
     })
-    it('Test Case 1 - Login success', () => {
+    it.only('Test Case 1 - Login success', () => {
         cy.get('#user-name').type('standard_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
         cy.get('[data-test="title"]').should('contain.text', 'Products');
     });
 
-    it('Test Case 2 - Login fail - empty username', () => {
+    it.skip('Test Case 2 - Login fail - empty username', () => {
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
         cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
     });
 
-    it('Test Case 3 - Login failed - locked user', () => {
+    it.skip('Test Case 3 - Login failed - locked user', () => {
         cy.get('#user-name').type('locked_out_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
