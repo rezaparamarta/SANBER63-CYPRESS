90a780bc42a1aeaa5dbf6c96cd1e5b73e99ff169
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-17 23:54:33 +0700

    Add some config and edit test script

--- cypress/e2e/web-test/saucedemo.cy.js
+++ cypress/e2e/web-test/saucedemo.cy.js
@@ -1,19 +1,24 @@
 describe('Test Suite 1', () => {
+    beforeEach(() => {
+        cy.visit('');
+    })
     it('Test Case 1 - Login success', () => {
-        cy.visit('https://www.saucedemo.com/');
         cy.get('#user-name').type('standard_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
+        cy.get('[data-test="title"]').should('contain.text', 'Products');
     });
+
     it('Test Case 2 - Login fail - empty username', () => {
-        cy.visit('https://www.saucedemo.com/');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
     });
+
     it('Test Case 3 - Login failed - locked user', () => {
-        cy.visit('https://www.saucedemo.com/');
         cy.get('#user-name').type('locked_out_user');
         cy.get('#password').type('secret_sauce');
         cy.get('#login-button').click();
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
     });
 });
