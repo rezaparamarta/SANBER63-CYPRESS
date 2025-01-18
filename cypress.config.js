cc5cff66140283211128fbb7e9462f1e416f1788
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-18 18:02:23 +0700

    Adding script and configuration and command

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com/',
+    env: {
+      userstg: 'user_staging',
+      userprod: 'user_production',
+    },
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportHeight: 650,
+    viewportWidth: 950,
+    defaultCommandTimeout: 4500,
+    screenshotOnRunFailure: false,
   },
 });
--- cypress/e2e/web-test/saucedemo.cy.js
+++ cypress/e2e/web-test/saucedemo.cy.js
@@ -2,10 +2,8 @@ describe('Saucedemo Login Scenarios', () => {
     beforeEach(() => {
         cy.visit('');
     })
-    it.only('Test Case 1 - Login success', () => {
-        cy.get('#user-name').type('standard_user');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
+    it('Test Case 1 - Login success', () => {
+        cy.loginSaucedemo('standard_user', 'secret_sauce');
         cy.get('[data-test="title"]').should('contain.text', 'Products');
     });
 
@@ -15,10 +13,17 @@ describe('Saucedemo Login Scenarios', () => {
         cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
     });
 
-    it.skip('Test Case 3 - Login failed - locked user', () => {
-        cy.get('#user-name').type('locked_out_user');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
+    it('Test Case 3 - Login failed - locked user', () => {
+        cy.loginSaucedemo('locked_out_user', 'secret_sauce');
+        // cy.get('#user-name').type('locked_out_user');
+        // cy.get('#password').type('secret_sauce');
+        // cy.get('#login-button').click();
         cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
     });
+
+    // it.only('Test Case 4 - Login env', () => {
+    //     cy.get('#user-name').type(Cypress.env('userstg'));
+    //     cy.get('#password').type('secret_sauce');
+    //     cy.get('#login-button').click();
+    // });
 });
